03c733a6ffab44b8454514e0a44555061362cfbf
Luke Bruni <31927302+LBruni98@users.noreply.github.com>
2017-11-22 11:50:55 +0000

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -14,16 +14,16 @@ As a user, I would like to choose the correct word from the list of words
 
 using namespace std;
 
-string getWords(string letter) {
+string getWords(string letters) {
 	string line;
 	string Dictionary; // Word Variable
 	int WordCounter; // Number to signify the amount of characters in the word
-  int MaxCoumter;
+  int MaxCounter; // The maximum amount of characters
   string result;
 
 	// Opens the file to be read
 	ifstream inFile;
-	inFile.open("Words.csv");
+	inFile.open("Words.txt");
 	
 	if(!inFile) { // If the file is not there , it returns a message...
 		cerr << "The file could not be read. Invalid type or the file is missing.";
@@ -31,41 +31,48 @@ string getWords(string letter) {
 	}
 	
 	if(inFile.is_open()) { // If the file is open...
-			while (inFile.good()) {//...and while the file is good
-				getline(inFile, line); // Read the lines
-				
-				for(int i=0; i<3; i++){
-					WordCounter = 0;
-         		for(int j=0; j<line.length();j++){
-         			if(letter[i] == line[j]){
-         				WordCounter++;
-         				break;
-         			}
-         		}
-         		
-         		if(WordCounter > MaxCoumter){
-         				result = line;
-         				MaxCoumter = WordCounter;
-         			}
+		while (inFile.good()) {//...and while the file is good
+			getline(inFile, line); // Read the lines
+			WordCounter = 0;
+			for(int i=0; i<letters.length(); i++) {
+				for(int j=0; j<line.length(); j++){
+					if(letters[i] == line[j]){
+         		WordCounter++;
+         		break;
+         	}
 				}
 			}
+			
+			if(WordCounter > MaxCounter){
+     			result = line;
+     			MaxCounter = WordCounter;
+				}
 		}
+	}
 	//STRING
 	//LIBRARY STRING IN C++
 	inFile.close(); // When done it closes the file
-	cout << "Result: " <<  result;
+	cout << "Result: " <<  result << endl;
 
 }
 
 int main() {
-	string letters; // The user's input
+  string letters; // The user's input
+
+while(true){
 	
-	while(letters.length()<=3) { // If the input length is under three letters, we continue
-		cout << "Enter Anagram (Max: 3):"; // 
+	  cout << "Enter Anagram (Max: 3):"; // 
 		getline(cin, letters);
-		cout << "Your Anagram: " << letters << "\n" ;
-		
-		getWords(letters); // The words that appear will come from the function above
+	
+	while(letters.length()>3) { // If the input length is above three letters
+		cout << "Too many characters in this input\n";
+		cout << "Enter Anagram Again(Max: 3):"; // 
+		getline(cin, letters);
 	}
+	
+	 cout << "Your Anagram: " << letters << "\n" ;
+	
+		getWords(letters); // The words that appear will come from the function above
+}
 	return 0;
 }
